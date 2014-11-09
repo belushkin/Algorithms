fa83e331d466b68643b1de8f76af0d218cceb8e0
belushkin <belushkin@belushkin-X550VB.(none)>
2014-11-10 00:00:03 +0200

    Adding recursion

---
+++ Recursion/factorial.php
@@ -0,0 +1,10 @@
+<?php
+
+function factorial($number)
+{
+    if ($number < 2) {
+        return 1;
+    } else {
+        return ($number * factorial($number - 1));
+    }
+}
---
+++ Recursion/fibonacci.php
@@ -0,0 +1,12 @@
+<?php
+
+function fibonacci($n)
+{
+    if ($n == 1 || $n == 2) {
+        return 1;
+    } else {
+        return fibonacci($n - 1) + fibonacci($n - 2);
+    }
+}
+
+print_r(fibonacci(12));

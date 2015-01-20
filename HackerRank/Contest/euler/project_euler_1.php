af1d06c48b6927d582c1e427b74cf9c9067cc225
Maxim Bielushkin <maxb@onehourtranslation.com>
2015-01-20 19:06:26 +0200

    Added new algo

--- HackerRank/Contest/euler/project_euler_1.php
+++ HackerRank/Contest/euler/project_euler_1.php
@@ -13,6 +13,7 @@ for ($i = 1; $i < $n+1; $i++) {
     echo euler($data[$i]), "\n";
 }
 
+// http://stackoverflow.com/questions/4587320/project-euler-1find-the-sum-of-all-the-multiples-of-3-or-5-below-1000
 // :Find the sum of all the multiples of 3 or 5 below 1000
 //For n=10; and k =3; ( 3 * (3+1) / 2) * 3
 function euler($n)

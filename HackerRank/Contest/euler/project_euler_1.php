30a8a41b76864736153cf5401b70b784c7693192
Maxim Bielushkin <maxb@onehourtranslation.com>
2015-01-20 19:05:23 +0200

    Added new algo

---
+++ HackerRank/Contest/euler/project_euler_1.php
@@ -0,0 +1,28 @@
+<?php
+
+$inputFile = fopen("project_euler_1.txt", "r") or die("Unable to open file!");
+
+$data = array();
+while(!feof($inputFile)) {
+    $data[] = trim(fgets($inputFile));
+}
+fclose($inputFile);
+
+$n = $data[0];
+for ($i = 1; $i < $n+1; $i++) {
+    echo euler($data[$i]), "\n";
+}
+
+// :Find the sum of all the multiples of 3 or 5 below 1000
+//For n=10; and k =3; ( 3 * (3+1) / 2) * 3
+function euler($n)
+{
+    $result = 0;
+    $k1     = 3;
+    $k2     = 5;
+
+//    $result = ()
+
+
+
+}

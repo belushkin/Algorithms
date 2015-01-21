062078601eb7308745a1b57d144a84225e99b02e
Maxim Bielushkin <maxb@onehourtranslation.com>
2015-01-21 10:59:29 +0200

    Added new algo

--- HackerRank/Contest/euler/project_euler_1.php
+++ HackerRank/Contest/euler/project_euler_1.php
@@ -9,23 +9,12 @@ while(!feof($inputFile)) {
 fclose($inputFile);
 
 $n = $data[0];
-//for ($i = 1; $i < $n+1; $i++) {
-    echo euler(1000,3) + euler(1000,5) - euler(1000,15), "\n";
-//}
+for ($i = 1; $i < $n+1; $i++) {
+    echo euler($data[$i],3) + euler($data[$i],5) - euler($data[$i],15), "\n";
+}
 
 function euler($k, $n)
 {
-    $m = ($k-1) / $n;
+    $m = intval(($k-1) / $n);
     return $n * $m * ($m+1) / 2;
 }
-
-
-//def sum_factors_of_n_below_k(k, n):
-//    m = (k-1) // n
-//    return n * m * (m+1) // 2
-//
-//def solution_01():
-//    return (sum_factors_of_n_below_k(1000, 3) +
-//        sum_factors_of_n_below_k(1000, 5) -
-//        sum_factors_of_n_below_k(1000, 15))
-
